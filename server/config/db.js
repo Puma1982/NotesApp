8b6fa26a62f40402ffb358619e935d45715bd8d6
Mario Fernández <nilovinilo1@gmail.com>
2023-04-18 18:52:37 +0200

    Bilded the Mongoos-DataBase Imported the file to the App made the configurations in .env and stared it also

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+/* to handle all the warnings in the terminal */
+mongoose.set("strictQuery", false);
+
+/* connecting to the DB */
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`DB connected to:${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
