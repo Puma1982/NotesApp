4a394482f97b358408bec2a15c310b388a250a91
Mario Fernández <nilovinilo1@gmail.com>
2023-04-21 19:15:22 +0200

    Created the Cards and style for them Have a Pagination Navigation item for looking at all the crated cards

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
  */
 
 exports.dashboard = async (req, res) => {
+  let perPage = 6;
+  let page = req.query.page || 1;
+
   const locals = {
     title: "Dashboard",
     description: "Auth.2 Calandar Note App",
@@ -14,12 +17,40 @@ exports.dashboard = async (req, res) => {
 
   try {
     /* in this tryCatch we are getting the notes from the DB and doing rendering  */
-    const notes = await Note.find({});
+    /* const notes = await Note.find({}); */
+
+    const notes = await Note.aggregate([
+      {
+        $sort: {
+          updatedAt: -1,
+        },
+      },
+      {
+        $match: { user: new mongoose.Types.ObjectId(req.user.id) },
+      },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      },
+    ])
+      /* Pagination */
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Note.count();
+
     res.render("dashboard/index", {
       userName: req.user.firstName,
       locals,
       notes,
       layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage),
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
